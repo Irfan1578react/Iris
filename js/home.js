71e366e862f8ca2cb4aaef6d61c0cf5c2de35353
Irfan1578react <ahamed6121@gmail.com>
2024-10-17 20:56:02 +0530

    comments were removed

--- js/home.js
+++ js/home.js
@@ -4,15 +4,13 @@ const pro=document.getElementsByClassName('li_product')[0];
 const lang=document.getElementsByClassName('language')[0];
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  navList.classList.toggle('active'); // Toggle 'active' class
+  navList.classList.toggle('active'); 
   pro.classList.toggle('active');
 });
 pro.addEventListener('click',() => {
   pro.classList.toggle('active');
 });
-// lang.addEventListener('click',() => {
-//   lang.classList.toggle('active');
-// });
+
 
 const container = document.querySelector('.iris_animation_bg');
 
@@ -21,40 +19,14 @@ window.addEventListener('scroll', () => {
   container.style.transform = `translateX(-${scrollPosition*0.5}px)`
 });
 window.addEventListener('scroll', function() {
-  const background = document.querySelector('.waves');  // Selects the element with class 'waves'
-  const scrollPosition = window.pageYOffset;  // Gets the vertical scroll position of the window
+  const background = document.querySelector('.waves');  
+  const scrollPosition = window.pageYOffset;  
   
-  background.style.transform = `translateY(-${scrollPosition * 0.5}px)`;  // Moves the element vertically
+  background.style.transform = `translateY(-${scrollPosition * 0.5}px)`;  
 });
 window.addEventListener('scroll', function() {
-  const background = document.querySelector('.wavefly');  // Selects the element with class 'waves'
-  const scrollPosition = window.pageYOffset; // Get the vertical scroll position
+  const background = document.querySelector('.wavefly');  
+  const scrollPosition = window.pageYOffset; 
       background.style.transform = `translate(-50%, -50%) rotate(-${scrollPosition * 0.2}deg)`;
- // Moves the element vertically
+ 
 });
-// function googleTranslateElementInit() {
-//   new google.translate.TranslateElement(
-//       {pageLanguage: 'en'},
-//       'google_translate_element'
-//   );
-// } 
-
-// const languageImg = document.querySelector('.language_img');
-// const googleTranslate = document.getElementById('google_translate_element');
-
-// languageImg.addEventListener('click', () => {
-//     // Toggle visibility using display style
-//     if (googleTranslate.style.display === 'none' || googleTranslate.style.display === '') {
-//         googleTranslate.style.display = 'block';
-//     } else {
-//         googleTranslate.style.display = 'none';
-//     }
-// });
-
-// const img = document.querySelector('.language_img');
-//     const checkbox = document.getElementById('toggle-checkbox');
-
-//     // Toggle the checkbox when the image is clicked
-//     img.addEventListener('click', () => {
-//         checkbox.checked = !checkbox.checked;
-//     });
